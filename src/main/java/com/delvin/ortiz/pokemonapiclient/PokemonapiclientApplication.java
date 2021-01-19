2c092413520515d978bb44fa40744315cd9bea0b
Delvin Ortiz <delvin732@gmail.com>
2021-01-18 21:13:25 -0500

    Project push

---
+++ src/main/java/com/delvin/ortiz/pokemonapiclient/PokemonapiclientApplication.java
@@ -0,0 +1,42 @@
+package com.delvin.ortiz.pokemonapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+
+import org.springframework.boot.CommandLineRunner;
+
+
+@SpringBootApplication
+public class PokemonapiclientApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(PokemonapiclientApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonapiclientApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder){
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception{
+
+		return args -> {
+
+			Pokemon pokemon = restTemplate.getForObject(
+				"https://pokeapi.co/api/v2/pokemon/1", Pokemon.class
+			);
+
+			log.info(pokemon.toString());
+
+		};
+	}
+}
---
+++ src/test/java/com/delvin/ortiz/pokemonapiclient/PokemonapiclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delvin.ortiz.pokemonapiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonapiclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
